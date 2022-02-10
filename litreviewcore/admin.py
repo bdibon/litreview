5424b27c836816a8fd65317189e8d247e66f856d
Boris Dibon <boris.dibon@gmail.com>
2022-02-10 17:03:13 +0100

    Customize admin UI for UserFollows

--- litreviewcore/admin.py
+++ litreviewcore/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import User, Ticket, Review, UserFollows
 
-admin.site.register(User, UserAdmin)
+# admin.site.register(User, UserAdmin)
+admin.site.register(Ticket)
+admin.site.register(Review)
+admin.site.register(UserFollows)
+
+
+class UserFollowsAnotherUserInline(admin.StackedInline):
+    model = UserFollows
+    extra = 1
+    fk_name = "user"
+    verbose_name = "l'utilisateur suit"
+    verbose_name_plural = "utilisateurs suivis"
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    inlines = [UserFollowsAnotherUserInline]
